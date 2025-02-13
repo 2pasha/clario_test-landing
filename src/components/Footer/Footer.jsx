4d2f3ccfe5eacd1935cd119fb71140a4b7445b0b
2pasha <kostishinpavlo24@gmail.com>
2025-02-13 18:29:01 +0200

    footer was implemented

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,44 @@
+import "./Footer.scss";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer container">
+      <div className="footer__links">
+        <a href="/" className="footer__link">
+          About
+        </a>
+        <a href="/" className="footer__link">
+          Privacy Notice
+        </a>
+        <a href="/" className="footer__link">
+          Company
+        </a>
+        <a href="/" className="footer__link">
+          EULA
+        </a>
+        <a href="/" className="footer__link">
+          Support
+        </a>
+        <a href="/" className="footer__link">
+          Uninstall
+        </a>
+        <a href="/" className="footer__link">
+          Cookie
+        </a>
+        <a href="/" className="footer__link">
+          Policy
+        </a>
+        <a href="/" className="footer__link">
+          Refund Policy
+        </a>
+      </div>
 
-export default Footer
+      <div className="footer__copyrights">
+        <p>Copyright ©</p>
+        <p>All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
